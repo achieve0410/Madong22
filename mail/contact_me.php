57bb6ca54c3ef11f750f4ee0cf5e4b3d94be4aa8
wonhyo <achieve0410@gmail.com>
2020-08-05 00:14:20 +0900

    gg

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,21 +1,21 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['message1']) || empty($_POST['message2']) || empty($_POST['message3']) || ) {
   http_response_code(500);
   exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$message1 = strip_tags(htmlspecialchars($_POST['message1']));
+$message2 = strip_tags(htmlspecialchars($_POST['message2']));
+$message3 = strip_tags(htmlspecialchars($_POST['message3']));
 
 // Create the email and send the message
 $to = "achieve0410@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$subject = "나는  $name 야띠-";
+$body = "나는 $message1 하고싶고, 내용은 $message2야. \n\n첨부는 $message3를 할게.";
 $header = "From: noreply@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";
+$header .= "Reply-To: $name";
 
 if(!mail($to, $subject, $body, $header)){
   http_response_code(500);
